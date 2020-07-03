3561d5fc49fdce76b9b56916d29f55d9bbb2e3fd
Philipp <poepsi39@gmail.com>
2020-07-03 09:51:59 +0200

    Add GPU utilization monitoring and implement instrument for X86

--- internal/telemd/instrument_test.go
+++ internal/telemd/instrument_test.go
@@ -74,6 +74,26 @@ func TestX86GpuFrequencyInstrument_MeasureAndReport(t *testing.T) {
 
 }
 
+func TestX86GpuUtilInstrument_MeasureAndReport(t *testing.T) {
+	instrument := X86GpuUtilInstrument{map[int]string{0: "dummy_gpu"}}
+	tc := telem.NewTelemetryChannel()
+
+	go instrument.MeasureAndReport(tc)
+
+	select {
+	case <-time.After(1 * time.Second):
+		t.Error("Timeout waiting for GPU utilization")
+	case t1 := <-tc.Channel():
+		if t1.Value < 0 {
+			t.Error("Unexpected negative GPU utilization")
+		}
+		log.Printf("%.4f\n", t1.Value)
+	}
+
+}
+
+
+
 func TestLoadInstrument_MeasureAndReport(t *testing.T) {
 	var instrument LoadInstrument
 	tc := telem.NewTelemetryChannel()
