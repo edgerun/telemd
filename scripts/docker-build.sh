e63d69daa2df39a1d48958881a7f24df3acab53b
Philipp <poepsi39@gmail.com>
2020-07-07 13:24:28 +0200

    Add GPU utilization monitoring and implement instrument for Jetson TX2

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -6,4 +6,4 @@
 BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT=$(realpath "${BASE}/../")
 
-docker build -t edgerun/telemd -f build/package/telemd/Dockerfile.amd64 .
+docker build -t edgerun/telemd:latest-aarch64 -f build/package/telemd/Dockerfile.aarch64 .
