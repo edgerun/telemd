50e1772090d7044028cc1daa8626f559f121127f
Philipp <poepsi39@gmail.com>
2020-12-04 19:54:41 +0100

    move gpu build into its own folder

--- scripts/docker-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# script to build x86 docker images for local usage.
-# we're assuming that you are using a an x86 machine.
-
-BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_ROOT=$(realpath "${BASE}/../")
-
-docker build -t edgerun/telemd:latest-aarch64 -f build/package/telemd/Dockerfile.aarch64 .
