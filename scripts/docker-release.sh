aa4bdb6794495370af5e33d7f43831ae973a1099
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2022-07-12 14:52:36 +0100

    Uncomment code for release

--- scripts/docker-release.sh
+++ scripts/docker-release.sh
@@ -17,26 +17,26 @@ cd $PROJECT_ROOT
 
 # build all the images
 docker build -t ${basetag}-amd64 -f build/package/telemd/Dockerfile.amd64 .
-#docker build -t ${basetag}-arm32v7 -f build/package/telemd/Dockerfile.arm32v7 .
-#docker build -t ${basetag}-aarch64 -f build/package/telemd/Dockerfile.aarch64 .
+docker build -t ${basetag}-arm32v7 -f build/package/telemd/Dockerfile.arm32v7 .
+docker build -t ${basetag}-aarch64 -f build/package/telemd/Dockerfile.aarch64 .
 
 # # push em all
 docker push ${basetag}-amd64 &
-#docker push ${basetag}-arm32v7 &
-#docker push ${basetag}-aarch64 &
+docker push ${basetag}-arm32v7 &
+docker push ${basetag}-aarch64 &
 
 wait
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 # create the manifest
-#docker manifest create ${basetag} \
-#	${basetag}-amd64 \
-#	${basetag}-arm32v7 \
-#	${basetag}-aarch64
+docker manifest create ${basetag} \
+	${basetag}-amd64 \
+	${basetag}-arm32v7 \
+	${basetag}-aarch64
 
 # explicit annotations
-#docker manifest annotate ${basetag} ${basetag}-arm32v7 --os "linux" --arch "arm" --variant "v7"
+docker manifest annotate ${basetag} ${basetag}-arm32v7 --os "linux" --arch "arm" --variant "v7"
 
 # ship it
-#docker manifest push --purge ${basetag}
+docker manifest push --purge ${basetag}
