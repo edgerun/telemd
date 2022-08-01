8f3208f8f3fa0e82faa4511eb745e519cb10123f
Philipp Raith <p.raith@dsg.tuwien.ac.at>
2022-08-01 22:26:13 +0100

    Rename aarch64 to arm64v8

--- scripts/gpu/docker-release.sh
+++ scripts/gpu/docker-release.sh
@@ -19,13 +19,11 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # build all the images
 docker build -t ${basetag}-amd64 -f build/package/telemd-gpu/Dockerfile.amd64 .
-docker build -t ${basetag}-arm32v7 -f build/package/telemd-gpu/Dockerfile.arm32v7 .
-docker build -t ${basetag}-aarch64 -f build/package/telemd-gpu/Dockerfile.aarch64 .
+docker build -t ${basetag}-arm64v8 -f build/package/telemd-gpu/Dockerfile.arm64v8 .
 
 # # push em all
 docker push ${basetag}-amd64 &
-docker push ${basetag}-arm32v7 &
-docker push ${basetag}-aarch64 &
+docker push ${basetag}-arm64v8 &
 
 wait
 
@@ -34,11 +32,11 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 # create the manifest
 docker manifest create ${basetag} \
 	${basetag}-amd64 \
-	${basetag}-arm32v7 \
-	${basetag}-aarch64
+	${basetag}-arm64v8
 
 # explicit annotations
-docker manifest annotate ${basetag} ${basetag}-arm32v7 --os "linux" --arch "arm" --variant "v7"
+docker manifest annotate ${basetag} ${basetag}-arm64v8 --os "linux" --arch "arm" --variant "v8"
+
 
 # ship it
 docker manifest push --purge ${basetag}
